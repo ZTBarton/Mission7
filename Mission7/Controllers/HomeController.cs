ff0d9e31e69561ee24fa45f41450d1c873908f23
Zachary Barton <75874562+ZTBarton@users.noreply.github.com>
2022-02-16 23:35:46 -0700

    Finished Pagination

--- Mission7/Controllers/HomeController.cs
+++ Mission7/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Mission7.Models;
+using Mission7.Models.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,29 @@ namespace Mission7.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private IBooksRepository repo;
+
+        public HomeController(IBooksRepository temp)
         {
-            return View();
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books.OrderBy(b => b.Title).Skip(pageSize * (pageNum - 1)).Take(pageSize),
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
         }
     }
 }
